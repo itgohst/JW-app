b20422caeb1dd338e82023effc66cbd118de57f6
itgohst <2830571618@qq.com>
2022-12-05 10:20:10 +0800

    feat: 完成修改1

---
+++ assets/NotFound.8201871e.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.a3048ab8.js";const o={};function n(r,e,s,c,_,a){return null}const f=t(o,[["render",n]]);export{f as default};
---
+++ assets/PersonalView.2a60cc1e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index.a3048ab8.js";const o={};function r(s,t){return c(),n("h1",null,"person")}const _=e(o,[["render",r]]);export{_ as default};
